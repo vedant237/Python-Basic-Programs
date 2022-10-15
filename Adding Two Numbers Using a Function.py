f3f9f3bf31302138cf6a546cbf8c8c71dd5aa2da
Vedant Chaudhari <114819383+vedant237@users.noreply.github.com>
2022-10-16 02:19:50 +0530

    Update Adding Two Numbers Using a Function.py

--- Adding Two Numbers Using a Function.py
+++ Adding Two Numbers Using a Function.py
@@ -1,13 +1,4 @@
-#-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
-#
-# Author:      VISHWANATH CHAUDHARI
-#
-# Created:     13-03-2022
-# Copyright:   (c) VISHWANATH CHAUDHARI 2022
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
+
 
 def add(a,b):      #def <function_name> (<parameters>
     c=a+b          #<tab>
