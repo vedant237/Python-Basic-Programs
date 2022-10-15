d370c2c0a4b2a21f969e1bf67ddac7f4cfc673e5
Vedant Chaudhari <114819383+vedant237@users.noreply.github.com>
2022-10-15 15:51:55 +0530

    Update Accessing Elements from Dictionary 1.py

--- Accessing Elements from Dictionary 1.py
+++ Accessing Elements from Dictionary 1.py
@@ -1,13 +1,4 @@
-#-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
-#
-# Author:      VISHWANATH CHAUDHARI
-#
-# Created:     13-03-2022
-# Copyright:   (c) VISHWANATH CHAUDHARI 2022
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
+
 
 Data = {'Name':'Vedant','Age':18,'Class':'I Year'}
 
