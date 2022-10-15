1fc76805d496e11b843dead316c4fa0d400eb97f
Vedant Chaudhari <114819383+vedant237@users.noreply.github.com>
2022-10-16 02:18:52 +0530

    Add files via upload

---
+++ Adding Two Numbers Using a Function.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      VISHWANATH CHAUDHARI
+#
+# Created:     13-03-2022
+# Copyright:   (c) VISHWANATH CHAUDHARI 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def add(a,b):      #def <function_name> (<parameters>
+    c=a+b          #<tab>
+    return c       #<tab>
+c = add(90,5)      #<tab>return<variable to be returned>
+print(c)
