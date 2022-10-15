8fd2f9250b6c61704a95e201ff069619fc410521
Vedant Chaudhari <114819383+vedant237@users.noreply.github.com>
2022-10-15 14:20:08 +0530

    Add files via upload

---
+++ Accessing Elements from Dictionary 1.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      VISHWANATH CHAUDHARI
+#
+# Created:     13-03-2022
+# Copyright:   (c) VISHWANATH CHAUDHARI 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+Data = {'Name':'Vedant','Age':18,'Class':'I Year'}
+
+print(Data.get('Name'))
+print(Data.get('Age'))
+
---
+++ Accessing Elements from Dictionary 2.py
@@ -0,0 +1,15 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      VISHWANATH CHAUDHARI
+#
+# Created:     13-03-2022
+# Copyright:   (c) VISHWANATH CHAUDHARI 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+capitals = {'USA':'Washington','UK':'London','Delhi':'India'}
+
+for value in capitals:
+    print( capitals[value] )
