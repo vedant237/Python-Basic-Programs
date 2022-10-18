39d5b4498b5282b832eb4cc7c897dcafc755606d
Vedant Chaudhari <114819383+vedant237@users.noreply.github.com>
2022-10-18 18:14:04 +0530

    Update Area of the Rectangle.py

--- Area of the Rectangle.py
+++ Area of the Rectangle.py
@@ -1,14 +1,3 @@
-#-------------------------------------------------------------------------------
-# Name:        Area of the Rectangle
-# Purpose:
-#
-# Author:      VEDANT CHAUDHARI
-#
-# Created:     23-12-2021
-# Copyright:   (c) VEDANT CHAUDHARI 2021
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
-
 L = input('Length of the Rectangle : ')
 L = float(L)
 B= input('Breadth of the Rectangle : ')
