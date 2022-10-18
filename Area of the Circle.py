08aada4bd18e6f2a394c306814d1658e82c8d879
Vedant Chaudhari <114819383+vedant237@users.noreply.github.com>
2022-10-18 18:12:36 +0530

    Add files via upload

---
+++ Area of the Circle.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# Name:        Area of the Circle
+# Purpose:
+#
+# Author:      VEDANT CHAUDHARI
+#
+# Created:     23-12-2021
+# Copyright:   (c) VEDANT CHAUDHARI 2021
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+Radius = input('The Radius of the Circle is : ' )
+Radius = float(Radius)
+Area = 3.14*(Radius**2)
+Area= float(Area)
+
+print('The Area of the Circle is : ', Area)
---
+++ Area of the Rectangle.py
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------------------------
+# Name:        Area of the Rectangle
+# Purpose:
+#
+# Author:      VEDANT CHAUDHARI
+#
+# Created:     23-12-2021
+# Copyright:   (c) VEDANT CHAUDHARI 2021
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+L = input('Length of the Rectangle : ')
+L = float(L)
+B= input('Breadth of the Rectangle : ')
+B = float(B)
+
+Area = L*B
+Area = float(Area)
+
+print('Area of the Rectangle : ', Area)
+
+
