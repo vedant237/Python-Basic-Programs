edf38d1a4ec580cb07531b3b6a3b98bd6557082b
Vedant Chaudhari <114819383+vedant237@users.noreply.github.com>
2022-10-19 00:37:42 +0530

    Add files via upload

---
+++ Area of Triangle.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        AREA OF TRIANGLE
+# Purpose:     To find Area of Triangle
+#
+# Author:      VEDANT CHAUDHARI
+#
+# Created:     07-01-2022
+# Copyright:   (c) VEDANT CHAUDHARI 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+B = float(input('Enter the Base of the Triangle :'))
+H = float(input('Enter the Height of the Triangle :'))
+
+Area = float(0.5*B*H)
+
+Ans = print('The Area of the Triangle is : ',Area)
