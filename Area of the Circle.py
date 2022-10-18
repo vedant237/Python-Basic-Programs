9cb2d40ac11f02e28dd2234e5b04cfd511e59fdb
Vedant Chaudhari <114819383+vedant237@users.noreply.github.com>
2022-10-18 18:13:03 +0530

    Update Area of the Circle.py

--- Area of the Circle.py
+++ Area of the Circle.py
@@ -1,13 +1,3 @@
-#-------------------------------------------------------------------------------
-# Name:        Area of the Circle
-# Purpose:
-#
-# Author:      VEDANT CHAUDHARI
-#
-# Created:     23-12-2021
-# Copyright:   (c) VEDANT CHAUDHARI 2021
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
 Radius = input('The Radius of the Circle is : ' )
 Radius = float(Radius)
 Area = 3.14*(Radius**2)
