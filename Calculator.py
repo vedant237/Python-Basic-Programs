a509984a02c5340137ae8292193f0c08a75f2dd6
Vedant Chaudhari <114819383+vedant237@users.noreply.github.com>
2022-10-20 21:08:09 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,14 +1,3 @@
-#-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
-#
-# Author:      VEDANT CHAUDHARI
-#
-# Created:     23-12-2021
-# Copyright:   (c) VEDANT CHAUDHARI 2021
-# Licence:     <your licence>
-#---------------------------------------------------------------------------
-
 First = input("Enter First Number : ")
 Operator = input("Enter Operator (+,-,*,/,%) : ")
 Second = input("Enter Second Number : ")
