40165d43697af9f8afab7a3d30af0114ba7eda63
Vedant Chaudhari <114819383+vedant237@users.noreply.github.com>
2022-10-22 00:21:26 +0530

    Update Celcius to Fahreinheit.py

--- Celcius to Fahreinheit.py
+++ Celcius to Fahreinheit.py
@@ -1,14 +1,3 @@
-#-------------------------------------------------------------------------------
-# Name:        Celcius to Fahrenheit
-# Purpose:
-#
-# Author:      VEDANT CHAUDHARI
-#
-# Created:     07-01-2022
-# Copyright:   (c) VEDANT CHAUDHARI 2022
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
-
 C = float(input('Enter the Temperature in Degree Celcius :'))
 F = (1.8*C) + 32
 
