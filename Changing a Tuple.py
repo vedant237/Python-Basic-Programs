7a020f75be2f4a61f49c8577a221f61f427af207
Vedant Chaudhari <114819383+vedant237@users.noreply.github.com>
2022-10-23 11:23:38 +0530

    Add files via upload

---
+++ Changing a Tuple.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      VISHWANATH CHAUDHARI
+#
+# Created:     12-03-2022
+# Copyright:   (c) VISHWANATH CHAUDHARI 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+tuple = (4,3,2,1, [7,8,5])
+
+tuple[4][0] = 3  #However, items of mutable element cannot be changed.But if the element is itself mutable datatype like list,its nested items can be changed
+print(tuple)
+
+tuple2 = ('p','y','t','h','o','n','p','r','o','g','r','a','m','s')
+print(tuple2)
+
+print((1,2,3)+(4,5,6))
+print(('Vedant')*3)
