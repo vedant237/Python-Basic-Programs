357826671e0a4368faa7077ee343ea7e35b65158
Vedant Chaudhari <114819383+vedant237@users.noreply.github.com>
2022-10-24 10:53:04 +0530

    Add files via upload

--- Changing Class Attribute.py
+++ Changing Class Attribute.py
@@ -1,3 +1,14 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      VISHWANATH CHAUDHARI
+#
+# Created:     22-01-2022
+# Copyright:   (c) VISHWANATH CHAUDHARI 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
 class Employee:
     company = 'Google'
 
---
+++ Check Num whether Palindrome or Not.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      VEDANT CHAUDHARI
+#
+# Created:     16-01-2022
+# Copyright:   (c) VEDANT CHAUDHARI 2022
+# Licence:     <your licence>
+#------------------------------------------------------------------------------
+
+class Number:
+    def sum(self):
+        return self.a + self.b
+
+num = Number()
+num.a = 14
+num.b = 6
+s = num.sum()
+print(s)
