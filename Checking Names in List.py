09364a1d0958d66eeb1ca0cd64eef29880eb7c10
Vedant Chaudhari <114819383+vedant237@users.noreply.github.com>
2022-10-25 14:08:08 +0530

    Update Checking Names in List.py

--- Checking Names in List.py
+++ Checking Names in List.py
@@ -1,14 +1,3 @@
-#-------------------------------------------------------------------------------
-# Name:        Check whether Object is present in the List or Not.
-# Purpose:
-#
-# Author:      VEDANT CHAUDHARI
-#
-# Created:     08-01-2022
-# Copyright:   (c) VEDANT CHAUDHARI 2022
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
-
 name = ['Vedant','Aarush','Vishwanath','Vaishali','Ram','Rahul','Raja','Rani']
 
 find = input('Enter the Name to Check :')
