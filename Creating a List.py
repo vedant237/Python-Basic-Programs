7d6512567b9f22cdb650cd06639ca58d5cddc409
Vedant Chaudhari <114819383+vedant237@users.noreply.github.com>
2022-10-26 17:57:52 +0530

    Update Creating a List.py

--- Creating a List.py
+++ Creating a List.py
@@ -1,14 +1,3 @@
-#-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
-#
-# Author:      VISHWANATH CHAUDHARI
-#
-# Created:     12-03-2022
-# Copyright:   (c) VISHWANATH CHAUDHARI 2022
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
-
 names = ['abc','def','ghi','jkl']
 orderItem = [1,'Jef','Computer',True]
 nestedList = ['mouse',[8,4,6],['a']]
