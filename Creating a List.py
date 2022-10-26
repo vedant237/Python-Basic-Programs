7046d08612aef36022091f92da18f207302dc2d9
Vedant Chaudhari <114819383+vedant237@users.noreply.github.com>
2022-10-26 17:57:03 +0530

    Add files via upload

---
+++ Creating a List.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      VISHWANATH CHAUDHARI
+#
+# Created:     12-03-2022
+# Copyright:   (c) VISHWANATH CHAUDHARI 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+names = ['abc','def','ghi','jkl']
+orderItem = [1,'Jef','Computer',True]
+nestedList = ['mouse',[8,4,6],['a']]
+
+print(names)
+print(orderItem)
+print(nestedList)
