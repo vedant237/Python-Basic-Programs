70c9479018f3c908680e956663eb0e160bd3c205
Vedant Chaudhari <114819383+vedant237@users.noreply.github.com>
2022-10-26 17:58:35 +0530

    Add files via upload

---
+++ Cube of the Number.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        Cube of the Number
+# Purpose:
+#
+# Author:      VEDANT CHAUDHARI
+#
+# Created:     23-12-2021
+# Copyright:   (c) VEDANT CHAUDHARI 2021
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+Number = input('Enter the Number : ')
+Number = float(Number)
+Cube = Number**3
+Cube = float(Cube)
+
+print('The Cube of the Number is : ',Cube)
+
+5
---
+++ Deciding Grade of Students.py
@@ -0,0 +1,34 @@
+#-------------------------------------------------------------------------------
+# Name:        Deciding Grade of Students
+# Purpose:
+#
+# Author:      VEDANT CHAUDHARI
+#
+# Created:     08-01-2022
+# Copyright:   (c) VEDANT CHAUDHARI 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+marks = int(input('Enter your Marks :'))
+
+if (marks>=90 and marks<=100):
+    print('The Student got Excellent Grade')
+
+elif (marks>=80 and marks<=90):
+    print('The Student got A  Grade')
+
+elif (marks>=70 and marks<=80):
+    print('The Student got B Grade')
+
+elif (marks>=60 and marks<=70):
+    print('The Student got C Grade')
+
+elif (marks>50 and marks<60):
+    print('The Student got C Grade')
+
+else:
+    print('The Student got F Grade')
+
+
+
+
