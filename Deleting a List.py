4f7ff084ddae900031f9b92c83521f7edffce9a8
Vedant Chaudhari <114819383+vedant237@users.noreply.github.com>
2022-10-27 16:34:23 +0530

    Update Deleting a List.py

--- Deleting a List.py
+++ Deleting a List.py
@@ -1,14 +1,3 @@
-#-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
-#
-# Author:      VISHWANATH CHAUDHARI
-#
-# Created:     12-03-2022
-# Copyright:   (c) VISHWANATH CHAUDHARI 2022
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
-
 charList = ['p','y','t','h','o','n','p','r','o','g','r','a','m','s']
 del charList[0]
 print(charList)
