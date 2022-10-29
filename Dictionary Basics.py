f0efed2791f17a06f82dcc1624e029297ac54ce7
Vedant Chaudhari <114819383+vedant237@users.noreply.github.com>
2022-10-29 14:15:41 +0530

    Update Dictionary Basics.py

--- Dictionary Basics.py
+++ Dictionary Basics.py
@@ -1,13 +1,2 @@
-#-------------------------------------------------------------------------------
-# Name:        Dictionary Basics
-# Purpose:     Learning Basics of Dictionary in Python
-#
-# Author:      VEDANT CHAUDHARI
-#
-# Created:     25-12-2021
-# Copyright:   (c) VEDANT CHAUDHARI 2021
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
-
 Marks= {'English':78, 'Chemistry':96, 'Maths':96,'SST':96,}
 print(Marks['Maths'])
