30b6e1b1b4560cae2418ab93773866fd502cdcc5
Vedant Chaudhari <114819383+vedant237@users.noreply.github.com>
2022-10-29 14:15:08 +0530

    Add files via upload

---
+++ Even or Odd Number.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      VEDANT CHAUDHARI
+#
+# Created:     12-01-2022
+# Copyright:   (c) VEDANT CHAUDHARI 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+Num = int(input('Enter the Number : '))
+
+if Num%2 == 0 :
+    print('The Given Number is Even.')
+else:
+        print('The Given Number is Odd.')
