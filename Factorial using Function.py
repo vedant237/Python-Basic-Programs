20019fa8deb59d4a3c9e84c0362047baeb87551a
Vedant Chaudhari <114819383+vedant237@users.noreply.github.com>
2022-10-30 18:34:13 +0530

    Update Factorial using Function.py

--- Factorial using Function.py
+++ Factorial using Function.py
@@ -1,14 +1,3 @@
-#-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
-#
-# Author:      VISHWANATH CHAUDHARI
-#
-# Created:     13-03-2022
-# Copyright:   (c) VISHWANATH CHAUDHARI 2022
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
-
 def factorial(n):
     fact = 1
     for no in range(1,n+1):
