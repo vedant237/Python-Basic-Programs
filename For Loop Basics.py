1b89bb94039ce46984de96fa2ff3d9ac536143a5
Vedant Chaudhari <114819383+vedant237@users.noreply.github.com>
2022-10-31 16:59:55 +0530

    Add files via upload

---
+++ For Loop Basics.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# Name:        For Loop Basics
+# Purpose:
+#
+# Author:      VEDANT CHAUDHARI
+#
+# Created:     25-12-2021
+# Copyright:   (c) VEDANT CHAUDHARI 2021
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+for Numbers in range(10):
+    print(Numbers) #Range in 0-9
+
+for Numbers1 in range(10):
+    print(Numbers1 + 1) #Range in 1-10
