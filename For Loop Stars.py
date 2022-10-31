7098b1c598b7e00608a0652dcb15f1fa2d50e51c
Vedant Chaudhari <114819383+vedant237@users.noreply.github.com>
2022-10-31 17:00:38 +0530

    Update For Loop Stars.py

--- For Loop Stars.py
+++ For Loop Stars.py
@@ -1,14 +1,2 @@
-#-------------------------------------------------------------------------------
-# Name: for loop
-
-# Purpose:
-#
-# Author:      VEDANTCHAUDHARI
-#
-# Created:     09-01-2022
-# Copyright:   (c) VEDANT CHAUDHARI 2022
-# Licence:     <your licence>
-#-----------------------------------------------------------------------------
-
 for i in range(4):
     print('*'* (i+1))
