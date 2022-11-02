f9ff923a8d06d18df192ca43723c7eb1f8a543ad
Vedant Chaudhari <114819383+vedant237@users.noreply.github.com>
2022-11-02 18:43:19 +0530

    Update Function to Return Values to Single Variable.py

--- Function to Return Values to Single Variable.py
+++ Function to Return Values to Single Variable.py
@@ -1,14 +1,3 @@
-#-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
-#
-# Author:      VISHWANATH CHAUDHARI
-#
-# Created:     13-03-2022
-# Copyright:   (c) VISHWANATH CHAUDHARI 2022
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
-
 def square_squareroot(a):
     square = a*a
     squareroot = a**0.5
