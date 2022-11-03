9a203215c3cb07a0522d7d42b7a68bda5e62b147
Vedant Chaudhari <114819383+vedant237@users.noreply.github.com>
2022-11-03 18:06:42 +0530

    Update Greetings to Specific Starting Letter Name.py

--- Greetings to Specific Starting Letter Name.py
+++ Greetings to Specific Starting Letter Name.py
@@ -1,14 +1,3 @@
-#-------------------------------------------------------------------------------
-# Name: Greetings Name Starts with specific letter
-# Purpose:
-#
-# Author:      VEDANT CHAUDHARI
-#
-# Created:     09-01-2022
-# Copyright:   (c) VEDANT CHAUDHARI 2022
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
-
 list = ['Rahul','Mohan','Sachin','Varun','Saurabh']
 
 for name in list:
