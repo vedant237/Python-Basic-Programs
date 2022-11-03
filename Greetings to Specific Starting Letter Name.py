abd64fa542e43b89d85786ea240938749c561dde
Vedant Chaudhari <114819383+vedant237@users.noreply.github.com>
2022-11-03 18:04:51 +0530

    Add files via upload

---
+++ Greetings to Specific Starting Letter Name.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name: Greetings Name Starts with specific letter
+# Purpose:
+#
+# Author:      VEDANT CHAUDHARI
+#
+# Created:     09-01-2022
+# Copyright:   (c) VEDANT CHAUDHARI 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+list = ['Rahul','Mohan','Sachin','Varun','Saurabh']
+
+for name in list:
+    if name.startswith('S'):
+        print('Hello '+name)
+
