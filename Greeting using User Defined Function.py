cc55473610c2e1a4304167d29791cbed1bbd5088
Vedant Chaudhari <114819383+vedant237@users.noreply.github.com>
2022-11-03 18:05:42 +0530

    Update Greeting using User Defined Function.py

--- Greeting using User Defined Function.py
+++ Greeting using User Defined Function.py
@@ -1,14 +1,3 @@
-#-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
-#
-# Author:      VEDANT CHAUDHARI
-#
-# Created:     11-01-2022
-# Copyright:   (c) VEDANT CHAUDHARI 2022
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
-
 def greet(name):
     print('Good Day '+ name)
 
