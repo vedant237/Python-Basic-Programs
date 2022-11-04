431ae86d1c8ef0a422e304b0fe0532af179c7439
Vedant Chaudhari <114819383+vedant237@users.noreply.github.com>
2022-11-04 21:44:44 +0530

    Update If-else 18 or Not.py

--- If-else 18 or Not.py
+++ If-else 18 or Not.py
@@ -1,14 +1,3 @@
-#-------------------------------------------------------------------------------
-# Name:        If-else for Age Greater than 18
-# Purpose:
-#
-# Author:      VEDANT CHAUDHARI
-#
-# Created:     07-01-2022
-# Copyright:   (c) VEDANT CHAUDHARI 2022
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
-
 Age = int(input('Enter your age : '))
 if(Age>18):
     print("You are an Adult")
