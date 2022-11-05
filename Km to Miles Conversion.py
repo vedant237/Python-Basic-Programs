0a89c757d1b3ae089a5eee4fd7f40315ae4409d6
Vedant Chaudhari <114819383+vedant237@users.noreply.github.com>
2022-11-05 18:39:55 +0530

    Update Km to Miles Conversion.py

--- Km to Miles Conversion.py
+++ Km to Miles Conversion.py
@@ -1,14 +1,3 @@
-#-------------------------------------------------------------------------------
-# Name:        Kilometre to Miles Conversion
-# Purpose:
-#
-# Author:      VEDANT CHAUDHARI
-#
-# Created:     07-01-2022
-# Copyright:   (c) VEDANT CHAUDHARI 2022
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
-
 K = float(input('Enter the Number in Kilometres : '))
 M = float(0.62* K)
 
