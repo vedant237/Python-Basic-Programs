794d0514d10bebeaba9ea6a97f250f452db0c723
Vedant Chaudhari <114819383+vedant237@users.noreply.github.com>
2022-11-06 17:02:04 +0530

    Add files via upload

---
+++ Leap Year or Not.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      VISHWANATH CHAUDHARI
+#
+# Created:     12-03-2022
+# Copyright:   (c) VISHWANATH CHAUDHARI 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+year = int(input('Enter the Year: '))
+
+if year % 4 == 0 :
+    print ('The Year you Entered is a Leap Year ! ')
+else :
+    print ('The Year you Entered is not a Leap Year !')
---
+++ List Indexing.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      VISHWANATH CHAUDHARI
+#
+# Created:     12-03-2022
+# Copyright:   (c) VISHWANATH CHAUDHARI 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+char_list = ['p','y','t','h','o','n']
+print(char_list[2])
+
+nestedList = ['Python Programs', [2,0,5,4,6,66]]
+print(nestedList[1][3])
+print(nestedList[0][7])
+print(nestedList[-1])
