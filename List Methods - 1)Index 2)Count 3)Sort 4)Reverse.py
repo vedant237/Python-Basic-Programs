34f9693a3cea6683ea6951b5e40fdd518aa06d9d
Vedant Chaudhari <114819383+vedant237@users.noreply.github.com>
2022-11-07 22:39:48 +0530

    Add files via upload

---
+++ List Methods - 1)Index 2)Count 3)Sort 4)Reverse.py
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      VISHWANATH CHAUDHARI
+#
+# Created:     13-03-2022
+# Copyright:   (c) VISHWANATH CHAUDHARI 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+list =[3,8,1,6,0,8,4,8]
+
+print(list.index(8))
+
+print(list.count(8))
+
+list.sort()
+print(list)
+
+list.reverse()
+print(list)
---
+++ List Methods - 1)Length 2)Min 3)Max.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      VISHWANATH CHAUDHARI
+#
+# Created:     13-03-2022
+# Copyright:   (c) VISHWANATH CHAUDHARI 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+list = [3,8,1,6,0,8,4,8,9,99]
+
+print(len(list))
+
+print(min(list))
+
+print(max(list))
