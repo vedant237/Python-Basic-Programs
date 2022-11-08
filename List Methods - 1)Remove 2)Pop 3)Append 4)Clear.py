0cd029002d42e3a225f0373676a21b51b908914c
Vedant Chaudhari <114819383+vedant237@users.noreply.github.com>
2022-11-08 17:23:45 +0530

    Add files via upload

---
+++ List Methods - 1)Remove 2)Pop 3)Append 4)Clear.py
@@ -0,0 +1,24 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      VISHWANATH CHAUDHARI
+#
+# Created:     13-03-2022
+# Copyright:   (c) VISHWANATH CHAUDHARI 2022
+# Licence:      your licence>
+#-------------------------------------------------------------------------------
+
+list = ['p','y','t','h','o','n']
+
+list.remove('p')
+print(list)
+
+list.pop(1)
+print(list)
+
+list.append('x')
+print(list)
+
+list.clear()
+print(list)
---
+++ List Slicing.py
@@ -0,0 +1,25 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      VISHWANATH CHAUDHARI
+#
+# Created:     12-03-2022
+# Copyright:   (c) VISHWANATH CHAUDHARI 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+charList =['p','y','t','h','o','n','s','l','i','c','i','n','g']
+print(charList[2:6])
+print(charList[:5])
+print(charList[5:])
+print(charList[:])
+
+for fruit in ['Banana','Apple','Orange','Chiku']:
+    print('I like',fruit)
+
+
+langList = ['C','Python','C++','Java']
+print('HTML' in langList)
+print('Java' in langList)
+
