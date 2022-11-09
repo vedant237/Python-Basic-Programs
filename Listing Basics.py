a94ea0ccc178d987b31ca49bacccfe9afff0f02c
Vedant Chaudhari <114819383+vedant237@users.noreply.github.com>
2022-11-09 20:16:13 +0530

    Add files via upload

---
+++ Listing Basics.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# Name:        Listing Basics
+# Purpose:
+#
+# Author:      VEDANT CHAUDHARI
+#
+# Created:     25-12-2021
+# Copyright:   (c) VEDANT CHAUDHARI 2021
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+Marks=[20,25,30]
+print(Marks[0]) #Output = Prints the Zeroth Number
+
+Marks1=[15,23,25,30]
+print(Marks1[1:3])  #Output = Prints the 1st and 2nd number..it excludes the ending number
---
+++ Membership Operators.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      VISHWANATH CHAUDHARI
+#
+# Created:     10-03-2022
+# Copyright:   (c) VISHWANATH CHAUDHARI 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+x = 'Python Programming'
+y = {1:'a', 2:'b'}
+
+print('P' in x)
+print('Python' not in x)
+
+print(1 in y)
+print('a' in y)
