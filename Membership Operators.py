a2879b78812a33d56f6f3e890765c682740047a7
Vedant Chaudhari <114819383+vedant237@users.noreply.github.com>
2022-11-09 20:17:16 +0530

    Update Membership Operators.py

--- Membership Operators.py
+++ Membership Operators.py
@@ -1,14 +1,3 @@
-#-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
-#
-# Author:      VISHWANATH CHAUDHARI
-#
-# Created:     10-03-2022
-# Copyright:   (c) VISHWANATH CHAUDHARI 2022
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
-
 x = 'Python Programming'
 y = {1:'a', 2:'b'}
 
