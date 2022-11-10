06940a68adb770f97290f2d723428fcc9f571b87
Vedant Chaudhari <114819383+vedant237@users.noreply.github.com>
2022-11-10 20:03:50 +0530

    Add files via upload

---
+++ Momentum of the Object.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      VISHWANATH CHAUDHARI
+#
+# Created:     23-01-2022
+# Copyright:   (c) VISHWANATH CHAUDHARI 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+m = float(input('Enter the Object Mass in Kilograms: '))
+c = float(input('Enter the Velocity in metre/sec: '))
+
+momentum = float(m*(c**2))
+
+answer = print('The Momentum of the Object is :',momentum)
