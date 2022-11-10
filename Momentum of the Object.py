6233e73ea7d2805b2dd8b151f30e6d9e6b7a6453
Vedant Chaudhari <114819383+vedant237@users.noreply.github.com>
2022-11-10 20:04:20 +0530

    Update Momentum of the Object.py

--- Momentum of the Object.py
+++ Momentum of the Object.py
@@ -1,14 +1,3 @@
-#-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
-#
-# Author:      VISHWANATH CHAUDHARI
-#
-# Created:     23-01-2022
-# Copyright:   (c) VISHWANATH CHAUDHARI 2022
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
-
 m = float(input('Enter the Object Mass in Kilograms: '))
 c = float(input('Enter the Velocity in metre/sec: '))
 
