817f2faf47b0a1702893506fb3d8635e49145b52
Vedant Chaudhari <114819383+vedant237@users.noreply.github.com>
2022-11-10 20:04:40 +0530

    Update module1.py

--- module1.py
+++ module1.py
@@ -1,14 +1,3 @@
-#-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
-#
-# Author:      VISHWANATH CHAUDHARI
-#
-# Created:     13-03-2022
-# Copyright:   (c) VISHWANATH CHAUDHARI 2022
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
-
 list = [3,8,1,6,0,8,4,8,9,99]
 
 print(len(list))
